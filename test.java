e7bd741f31f606e8b648cb6d5b7acfaa531e5008
yitian <dragon_michael19@hotmail.com>
2015-07-27 23:18:46 +0100

    add java test file

---
+++ test.java
@@ -0,0 +1,70 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+		int value = 0;
+
+		while (value < 10) {
+			System.out.println(value);
+			value += 1;
+		}
+
+		int i = 0;
+
+		for (i = 0; i < 10; i++) {
+			System.out.println(i);
+
+		}
+
+		int x = 0;
+		int y = 1;
+		if (x > y) {
+			System.out.println("se");
+
+		} else if (x == y) {
+			System.out.println("xx");
+
+		} else {
+			System.out.println("re");
+
+		}
+		// system input Scanner
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("return");
+		int value1 = input.nextInt();
+		// String line = input.nextLine();
+
+		System.out.println(value1);
+
+		int testvalue = 0;
+
+		do {
+			System.out.println(testvalue);
+			testvalue += 1;
+		} while (testvalue < 5);
+  
+		System.out.println("test switch");
+		
+		 
+		Scanner input1 = new Scanner(System.in); 
+		int text = input1.nextInt(); 
+		
+		switch (text) {
+		case 3:
+			System.out.println("xx");
+			break;
+		default:
+			System.out.println("test");
+		
+		}; 
+
+	}
+
+}
